ddb6d787626caa0cbc99448de862de1d0ab0805d
amnimhops <manuel.lillo@gmail.com>
2016-02-26 09:25:32 +0100

    changed poly count

--- bestcad/bestcad.js
+++ bestcad/bestcad.js
@@ -170,7 +170,7 @@ function SceneManager(config){
 		gl.useProgram(program);
 		gl.viewport(0,0,800,600);
 		
-		createTriangles(10000);
+		createTriangles(100000);
 
     	var vPosition = gl.getAttribLocation( program, "vPosition" );
         gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 12 * 4, 0 ); // offset  8 elementos * 4 bytes, desplazamiento 0
@@ -220,8 +220,8 @@ gl.clearColor(0.4,0.4,0.4,1);
 		gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
 		gl.uniformMatrix4fv( uniformRotation ,false, rotationMatrix);
 
-		//gl.drawArrays(gl.TRIANGLES, 0, 20000*3);
-		gl.drawElements(gl.TRIANGLES, 10000*3, gl.UNSIGNED_SHORT, 0);
+		gl.drawArrays(gl.TRIANGLES, 0, 100000*3);
+		//gl.drawElements(gl.TRIANGLES, 100000*3, gl.UNSIGNED_SHORT, 0);
 
 		//gl.lineWidth(3);
 		//gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
